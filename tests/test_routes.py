8c7a5ee18809a339fb53901f5dbbbb7cb125db95
Golorun <golorun@example.com>
2023-09-12 15:03:41 -0400

    correcting typo in test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -135,6 +135,6 @@ class TestAccountService(TestCase):
         self.assertEqual(data["name"], account.name)
 
     def test_get_account_not_found(self):
-        """"It should not Read an account that is not found"""
+        """It should not Read an account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
